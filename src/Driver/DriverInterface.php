9a8a8823bddde92b23edf9b657548d3b5e528035
nsnake <root@playcat.net>
2024-01-09 14:04:17 +0800

    fix：bug

--- src/Driver/DriverInterface.php
+++ src/Driver/DriverInterface.php
@@ -3,6 +3,8 @@
 namespace Playcat\Queue\Driver;
 
 use Playcat\Queue\Model\Payload;
+use Playcat\Queue\Protocols\ConsumerDataInterface;
+use Playcat\Queue\Protocols\ProducerDataInterface;
 
 interface DriverInterface
 {
