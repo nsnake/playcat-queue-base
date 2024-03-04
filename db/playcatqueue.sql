36bdec22308565e9b161063ef404369738c80c83
nsnake <root@playcat.net>
2024-03-04 17:21:31 +0800

    统一数据库创建

--- db/playcatqueue.sql
+++
@@ -1,9 +0,0 @@
-DROP TABLE IF EXISTS `jobs`;
-CREATE TABLE `jobs`  (
-  `jid` int(0) UNSIGNED NOT NULL AUTO_INCREMENT,
-  `timerid` int(0) NOT NULL DEFAULT -1,
-  `iconicid` int(0) NOT NULL DEFAULT -1,
-  `data` blob NULL,
-  `expiration` int(0) UNSIGNED NOT NULL DEFAULT 0,
-  PRIMARY KEY (`jid`) USING BTREE
-) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
--- src/Install/InitDB.php
+++ src/Install/InitDB.php
@@ -11,56 +11,59 @@
 
 namespace Playcat\Queue\Install;
 
-use think\db\Query;
 use think\DbManager;
-use think\Exception;
 
 class InitDB
 {
     private $db;
+    private $config;
 
     /**
      * @param array $config
      * @return void
      */
-    public function setDriver(array $config): void
+    public function __construct(array $config)
     {
-        $db = new DbManager();
-        $db->setConfig([
-            'default' => $config['type'],
-            'connections' => [
-                $config['type'] => $config
-            ]
-        ]);
-        $this->db = new Query($db->connect());
+        $this->config = $config;
     }
 
     /**
      * For mysql sql.
-     * @return void
-     */
-    public function initMysql()
-    {
-        $sql = 'DROP TABLE IF EXISTS `jobs`;CREATE TABLE `jobs`  (`jid` int(0) UNSIGNED NOT NULL AUTO_INCREMENT,`timerid` int(0) NOT NULL DEFAULT -1,`iconicid` int(0) NOT NULL DEFAULT -1,`data` blob NULL,`expiration` int(0) UNSIGNED NOT NULL DEFAULT 0,PRIMARY KEY (`jid`) USING BTREE) ENGINE = InnoDB ROW_FORMAT = Dynamic';
-        $this->queue($sql);
-    }
-
-    /**
-     * For sqlite file
-     * @return void
-     */
-    public function initSqlite()
-    {
-        $sql = 'DROP TABLE IF EXISTS "jobs";CREATE TABLE "jobs" ("jid" integer NOT NULL PRIMARY KEY AUTOINCREMENT,"timerid" integer NOT NULL DEFAULT 0,"iconicid" integer NOT NULL DEFAULT 0,"data" blob,"expiration" integer NOT NULL DEFAULT 0)';
-        $this->queue($sql);
-    }
-
-    /**
-     * @param string $sql
      * @return bool
      */
-    private function queue(string $sql): bool
+    public function initMysql(): bool
     {
-        return $this->db->batchQuery($sql);
+        $db = new DbManager();
+        $db->setConfig([
+            'default' => $this->config['type'],
+            'connections' => [
+                $this->config['type'] => $this->config
+            ]
+        ]);
+        $this->db = $db->connect();
+
+        $sql = 'CREATE TABLE `jobs`  (`jid` int(0) UNSIGNED NOT NULL AUTO_INCREMENT,`timerid` int(0) NOT NULL DEFAULT -1,`iconicid` int(0) NOT NULL DEFAULT -1,`data` blob NULL,`expiration` int(0) UNSIGNED NOT NULL DEFAULT 0,PRIMARY KEY (`jid`) USING BTREE) ENGINE = InnoDB;';
+
+        $is_success = true;
+        try {
+            $this->db->execute($sql);
+        } catch (\Exception $e) {
+            $is_success = false;
+        }
+        return $is_success;
     }
+
+    /**
+     * @return bool
+     */
+    public function initSqlite(): bool
+    {
+        $sql = 'CREATE TABLE "jobs" ("jid" integer NOT NULL PRIMARY KEY AUTOINCREMENT,"timerid" integer NOT NULL DEFAULT 0,"iconicid" integer NOT NULL DEFAULT 0,"data" blob,"expiration" integer NOT NULL DEFAULT 0);';
+        $pdo = new \PDO('sqlite:' . $this->config['database']);
+        $result = $pdo->exec($sql);
+        return $result === 0 ? true : false;
+
+    }
+
+
 }
