3d6428c7615a0c71343a09883dc4afd43c25256c
nsnake <root@playcat.net>
2024-05-22 14:03:27 +0800

    add:日志功能

--- src/Log/Log.php
+++ src/Log/Log.php
@@ -11,6 +11,26 @@
 
 namespace Playcat\Queue\Log;
 
+/**
+ * @method static debug(string $message, array $context = []);
+调试信息
+ * @method static info(string $message, array $context = []);
+信息
+ * @method static notice(string $message, array $context = []);
+通知
+ * @method static warning(string $message, array $context = []);
+警告
+ * @method static error(string $message, array $context = []);
+一般错误
+ * @method static critical(string $message, array $context = []);
+危险错误
+ * @method static alert(string $message, array $context = []);
+警戒错误
+ * @method static emergency(string $message, array $context = []);
+紧急错误
+ *
+ * @see LogDriverInterface
+ */
 class Log
 {
     private static $log_handle;
